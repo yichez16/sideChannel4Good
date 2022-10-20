abcf19b225845089bb854cb81dfc0c075490b679
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2022-10-19 22:18:43 -0700

    1

--- examples/CUPTI_Conv_Event.cu
+++ examples/CUPTI_Conv_Event.cu
@@ -127,7 +127,7 @@ free(C);
 
 int main()  
 {
-freopen(path_0,"w",stdout);
+// freopen(path_0,"w",stdout);
 
 using namespace std;
 CUdevice device;
@@ -191,6 +191,10 @@ for(int i=0;i<10;i++)
 	gettimeofday(&ts,NULL);
 	
 	compute();
+	compute();
+	compute();
+	compute();
+
 	p->stop();
 	gettimeofday(&te,NULL);
 
@@ -204,7 +208,7 @@ for(int i=0;i<10;i++)
 
 
   
-fclose(stdout);
+// fclose(stdout);
 return 0;
 }
 
