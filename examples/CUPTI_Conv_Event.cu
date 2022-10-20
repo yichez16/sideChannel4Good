2c5b13c4b0c25d2ee44c40c7ed2ce62f2289461e
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2022-10-20 01:22:23 -0700

    25

--- examples/CUPTI_Conv_Event.cu
+++ examples/CUPTI_Conv_Event.cu
@@ -350,7 +350,7 @@ cuCtxCreate(&context, 0, 0);
 
 for(int i=0;i<30;i++)
 {
-	for(int j=0;j<50;j++)
+	for(int j=0;j<25;j++)
 	{
 	cupti_profiler::profiler *p= new cupti_profiler::profiler(event_names, metric_names, context);
 	struct timeval ts,te;
