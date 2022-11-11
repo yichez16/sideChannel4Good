760852c82b2370b6fec9e1ccd82f49802bd0ad51
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2022-11-10 22:48:16 -0800

    Update CUPTI_Conv_Event.cu

--- examples/CUPTI_Conv_Event.cu
+++ examples/CUPTI_Conv_Event.cu
@@ -293,7 +293,7 @@ const auto event_names = cupti_profiler::available_events(device);
 const auto metric_names = cupti_profiler::available_metrics(device);
 #else
   vector<string> event_names {    
-    "fb_subp0_read_sectors",
+    "fb_subp0_write_sectors",
     "l2_subp0_read_tex_hit_sectors",
     "tex0_cache_sector_queries",
     "inst_executed",
