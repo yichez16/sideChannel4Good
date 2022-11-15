34995ab64f98bb1b5fc2510fabe4c3a84e8b2586
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2022-11-15 10:16:26 -0800

    Update CUPTI_Conv_Event.cu

--- examples/CUPTI_Conv_Event.cu
+++ examples/CUPTI_Conv_Event.cu
@@ -278,8 +278,6 @@ static void compute_mat() {
     // cudaStream_t stream1, stream2;
     // cudaStreamCreate(&stream1); cudaStreamCreate(&stream2); 
 
-    cupti_profiler::profiler *p1= new cupti_profiler::profiler(event_names, metric_names, context);
-    struct timeval ts1,te1;    
 
 
     for (int j = 0; j < 10; j++) {
@@ -303,6 +301,8 @@ static void compute_mat() {
     // struct timeval ts,te;
     
 
+    cupti_profiler::profiler *p1= new cupti_profiler::profiler(event_names, metric_names, context);
+    struct timeval ts1,te1;    
 
     // CUcontext context1;
     // cuCtxCreate(&context1, 0, 0);
