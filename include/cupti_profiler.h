2a8e370e65d7b82587c789913e8b3fb7a3708f49
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2022-11-14 21:39:28 -0800

    Update cupti_profiler.h

--- include/cupti_profiler.h
+++ include/cupti_profiler.h
@@ -574,8 +574,9 @@ namespace detail {
           else
             s << (ull_t)m_kernel_data[k.first].m_event_values[i]
               << ","
-              << ts.tv_sec*1000000 + ts.tv_usec
-              << "," << te.tv_sec*1000000 + te.tv_usec
+              << (te.tv_sec - ts.tv_sec)*1000000 + te.tv_usec - ts.tv_usec
+              // << ts.tv_sec*1000000 + ts.tv_usec
+              // << "," << te.tv_sec*1000000 + te.tv_usec
               << ",";
         }
         s << kernel_separator;
