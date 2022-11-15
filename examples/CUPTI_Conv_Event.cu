c42b0b1938c662fd99813c2f90b92e190fb8bc7b
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2022-11-15 11:03:48 -0800

    Update CUPTI_Conv_Event.cu

--- examples/CUPTI_Conv_Event.cu
+++ examples/CUPTI_Conv_Event.cu
@@ -310,20 +310,20 @@ static void compute_mat() {
 
 
 
-    for (int j = 0; j < 1; j++) {
+    // for (int j = 0; j < 1; j++) {
 
-        p->start();
-        gettimeofday(&ts,NULL);
-        for (int i = 0; i < 1; i++) {
-        matMul<<<32,128>>>(d_A, d_B, d_C, numARows, numACols, numBCols);
-        }
-        p->stop();
-        gettimeofday(&te,NULL);
+    //     p->start();
+    //     gettimeofday(&ts,NULL);
+    //     for (int i = 0; i < 1; i++) {
+    //     matMul<<<32,128>>>(d_A, d_B, d_C, numARows, numACols, numBCols);
+    //     }
+    //     p->stop();
+    //     gettimeofday(&te,NULL);
 
-        p->print_event_values(std::cout,ts,te);
-        // p->print_metric_values(std::cout,ts,te);
-        // p->print_events_and_metrics(std::cout);
-    }
+    //     p->print_event_values(std::cout,ts,te);
+    //     // p->print_metric_values(std::cout,ts,te);
+    //     // p->print_events_and_metrics(std::cout);
+    // }
     
 
 
